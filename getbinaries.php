3623d9f30cabc578fe87ca985954f586a29a6246
hooftly <hooftly@TINDEVM001.tf-dev.org>
2019-11-16 13:10:38 -0700

    fix derping getbinaries.php

--- getbinaries.php
+++ getbinaries.php
@@ -4,12 +4,12 @@ require_once('./assets/version.php');
 $x = new stdClass();
 $x->cli_version = CLI_VERSION . ": " . CLI_CODENAME;
 $x->binary_url = "https://getamitycoin.org/assets/";
-$x->windows = "Amity-"  CLI_VERSION . "-WIN64.zip";
-$x->linux = "Amity-"  CLI_VERSION . "-LINUX64.zip";
-$x->mac = "Amity-"  CLI_VERSION . "-MACOS64.zip";
-$x->armhf = "Amity-"  CLI_VERSION . "-ARMHF.zip";
-$x->aarch64 = "Amity-"  CLI_VERSION . "-ARM64.zip";
-$x->risc = "Amity-"  CLI_VERSION . "-RISCV64.zip";
+$x->windows = "Amity-" . CLI_VERSION . "-WIN64.zip";
+$x->linux = "Amity-" . CLI_VERSION . "-LINUX64.zip";
+$x->mac = "Amity-" . CLI_VERSION . "-MACOS64.zip";
+$x->armhf = "Amity-" . CLI_VERSION . "-ARMHF.zip";
+$x->aarch64 = "Amity-" . CLI_VERSION . "-ARM64.zip";
+$x->risc = "Amity-" . CLI_VERSION . "-RISCV64.zip";
 
 echo json_encode($x, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 ?>
